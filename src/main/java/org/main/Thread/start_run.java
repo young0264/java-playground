c3d9cd01b9ad83a034e68338b8b93717c6417ee3
남의영 <ny2485@naver.com>
2023-10-31 14:46:22 +0900

    thread.start() vs thread.run() 차이 테스트

---
+++ src/main/java/org/main/Thread/start_run.java
@@ -0,0 +1,20 @@
+package org.main.Thread;
+
+/**
+ * https://kim-jong-hyun.tistory.com/101
+ * thread.start() vs thread.run() 차이 분석
+ * run() : 새로운 call stack을 생성하지 않고 현재 call stack에 새로운 thread가 실행 (싱글스레드)
+ * start() : 새로운 call stack을 생성하고, 새로운 thread가 그 위에서 실행 (멀티스레드)
+ * */
+public class start_run {
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[5];
+        System.out.println("thread start : " + Thread.currentThread().getName());
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> System.out.println("thread run start : " + Thread.currentThread().getName()));
+//            threads[i].start();
+            threads[i].run();
+        }
+    }
+}
