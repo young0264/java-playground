63f56a16186e94c73e17dd4ea510e9467c7f9338
남의영 <ny2485@naver.com>
2023-10-31 14:45:51 +0900

    Initial commit

---
+++ src/main/java/org/main/Main.java
@@ -0,0 +1,12 @@
+package org.main;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+//    Executor
+//    ExecutorService
+}
