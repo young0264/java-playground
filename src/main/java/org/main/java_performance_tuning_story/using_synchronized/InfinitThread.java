da21e51ad3d31527f26febf4eed768efce47e025
남의영 <ny2485@naver.com>
2024-03-20 00:28:03 +0900

    How interrupted() method work exam

---
+++ src/main/java/org/main/java_performance_tuning_story/using_synchronized/InfinitThread.java
@@ -0,0 +1,16 @@
+package org.main.java_performance_tuning_story.using_synchronized;
+
+public class InfinitThread extends Thread {
+    int value = Integer.MIN_VALUE;
+    private boolean flag = true;
+
+    public void run() {
+        while (flag) {
+            value++;
+            if (value == Integer.MAX_VALUE) {
+                value = Integer.MIN_VALUE;
+                System.out.println("MAX_VALUE 도달 ~!");
+            }
+        }
+    }
+}
---
+++ src/main/java/org/main/java_performance_tuning_story/using_synchronized/InterruptSample.java
@@ -0,0 +1,12 @@
+package org.main.java_performance_tuning_story.using_synchronized;
+
+public class InterruptSample {
+    public static void main(String[] args) throws InterruptedException {
+        InfinitThread infinitThread = new InfinitThread();
+        infinitThread.start();
+        Thread.sleep(2000);
+        System.out.println("isInterrupted = " + infinitThread.isInterrupted());
+        infinitThread.interrupt(); // interrupt는 특정 상황에서 동작.
+        System.out.println("isInterrupted = " + infinitThread.isInterrupted());
+    }
+}
