38568cff6bb76b8427124d47aa06afd8f5565a8a
남의영 <ny2485@naver.com>
2024-03-20 01:06:56 +0900

    making synchronized issue

--- src/main/java/org/main/java_performance_tuning_story/using_synchronized/ContributeTest.java
+++ src/main/java/org/main/java_performance_tuning_story/using_synchronized/ContributeTest.java
@@ -3,10 +3,10 @@ package org.main.java_performance_tuning_story.using_synchronized;
 public class ContributeTest {
     public static void main(String[] args) {
         Contributor[] crs = new Contributor[10];
+        Contribution contribution = new Contribution();
 
         //기부자와 기부 단체 초기화
         for (int loop = 0; loop < 10; loop++) {
-            Contribution contribution = new Contribution();
             crs[loop] = new Contributor(contribution, " Contributor" + loop);
         }
 
