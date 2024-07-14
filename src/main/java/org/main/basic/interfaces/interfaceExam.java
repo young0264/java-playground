9b438b0be75c7b09efd04de637d7a4f6ffaba123
남의영 <ny2485@naver.com>
2024-07-14 23:08:19 +0900

    add interface exam

---
+++ src/main/java/org/main/basic/interfaces/interfaceExam.java
@@ -0,0 +1,15 @@
+package org.main.basic.interfaces;
+
+public interface interfaceExam {
+
+    public int MAX_VOLUME = 10;
+    public String t1 = "123";
+
+    public default void t1() {
+
+    }
+    public static void t2() {
+
+    }
+
+}
---
+++ src/test/java/org/main/basic/InterfaceTest.java
@@ -0,0 +1,9 @@
+package org.main.basic;
+
+import org.junit.jupiter.api.Test;
+
+public class InterfaceTest {
+
+//    @Test
+//    void
+}
