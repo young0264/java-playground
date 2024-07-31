e7aa14b362ad80965db87840a53af4a25174f15f
남의영 <ny2485@naver.com>
2024-07-31 13:12:11 +0900

    generics do not support type conversion in java

--- src/main/java/org/main/basic/generic/VarianceWithGeneric.java
+++ src/main/java/org/main/basic/generic/VarianceWithGeneric.java
@@ -5,14 +5,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class VarianceWithGeneric {
-    public static void print(List<Object> arr) {
+    public static void print(List<Integer> arr) {
         for (Object obj : arr) {
             System.out.println(obj);
         }
     }
+//    public static void print(List arr) {
+//        for (Integer obj : arr) {
+//            System.out.println(obj);
+//        }
+//    }
 
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(1, 2, 3);
-//        print(integers); // ! 제네릭에 선언하는 type은 캐스팅이 안됨.
+        print(integers); // ! 제네릭에 선언하는 type은 캐스팅이 안됨.
     }
 }
