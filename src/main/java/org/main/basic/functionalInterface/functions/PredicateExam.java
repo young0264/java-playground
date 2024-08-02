4f153eb43a28d72501ffcad29490a57aaef5bcce
남의영 <ny2485@naver.com>
2024-08-02 17:58:47 +0900

    add comment syntax about Predicate combination interface

--- src/main/java/org/main/basic/functionalInterface/functions/PredicateExam.java
+++ src/main/java/org/main/basic/functionalInterface/functions/PredicateExam.java
@@ -1,53 +1,29 @@
 package org.main.basic.functionalInterface.functions;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
-/**
- * 인터페이스 명 : Predicate<T>
- * 추상메서드 명 : boolean test(T t);
- * API 설명 : 매개값이 조건에 맞는지 평가하여 boolean return
- **/
 public class PredicateExam {
     public static void main(String[] args) {
-        Kitchen[] kitchens = {
-                new Kitchen("Korean", 80),
-                new Kitchen("Chinese", 100),
-                new Kitchen("Japanes", 50),
-                new Kitchen("American", 150),
-                new Kitchen("European", 100)
-        };
 
-        IntPredicate isPass = (info) -> {
-            return info > 100;
-        };
+        final int MIN_NUM = 10;
+        final int MAX_NUM = 20;
+        final int THRESHOLD_5 = 5;
+        final int THRESHOLD_15 = 15;
+        final int THRESHOLD_50 = 50;
 
-        List<Kitchen> kitchenInfos = Arrays.asList(kitchens);
+        Predicate<Integer> greater = x -> x > MIN_NUM;
+        Predicate<Integer> less = x -> x < MAX_NUM;
 
-        for (Kitchen kitchenInfo : kitchenInfos) {
-            String type = kitchenInfo.type;
-            int capacity = kitchenInfo.capacity;
+        // x > MIN_NUM && x < MAX_NUM
+        Predicate<Integer> between = greater.and(less);
+        System.out.println("between : " + between.test(THRESHOLD_15)); // true
 
-            boolean isPassedCapacity = isPass.test(capacity);
+        // x > MIN_NUM || x < MAX_NUM
+        Predicate<Integer> all = greater.or(less);
+        System.out.println("all : " + all.test(THRESHOLD_5)); // true
 
-            if (isPassedCapacity) {
-                System.out.println(type + "주방은 수용가능한 인력을 갖춘 주방입니다.");
-            } else {
-                System.out.println(type+"주방은 수용가능한 인력을 갖추지 못했습니다.");
-            }
-        }
-
-    }
-
-    static class Kitchen{
-        String type;
-        int capacity;
-
-        public Kitchen(String type, int capacity) {
-            this.type = type;
-            this.capacity = capacity;
-        }
+        // x <= MIN_NUM
+        Predicate<Integer> negate = greater.negate();
+        System.out.println("negate : " + negate.test(THRESHOLD_50)); // false
     }
 }
---
+++ src/main/java/org/main/basic/functionalInterface/functions/XXXPredicateExam.java
@@ -0,0 +1,51 @@
+package org.main.basic.functionalInterface.functions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
+
+/**
+ * 인터페이스 명 : Predicate<T>
+ * 추상메서드 명 : boolean test(T t);
+ * API 설명 : 매개값이 조건에 맞는지 평가하여 boolean return
+ **/
+public class XXXPredicateExam {
+    public static void main(String[] args) {
+        Kitchen[] kitchens = {
+                new Kitchen("Korean", 80),
+                new Kitchen("Chinese", 100),
+                new Kitchen("Japanes", 50),
+                new Kitchen("American", 150),
+                new Kitchen("European", 100)
+        };
+
+        IntPredicate isPass = (info) -> {
+            return info > 100;
+        };
+
+        List<Kitchen> kitchenInfos = Arrays.asList(kitchens);
+
+        for (Kitchen kitchenInfo : kitchenInfos) {
+            String type = kitchenInfo.type;
+            int capacity = kitchenInfo.capacity;
+
+            boolean isPassedCapacity = isPass.test(capacity);
+
+            if (isPassedCapacity) {
+                System.out.println(type + "주방은 수용가능한 인력을 갖춘 주방입니다.");
+            } else {
+                System.out.println(type+"주방은 수용가능한 인력을 갖추지 못했습니다.");
+            }
+        }
+    }
+
+    static class Kitchen{
+        String type;
+        int capacity;
+
+        public Kitchen(String type, int capacity) {
+            this.type = type;
+            this.capacity = capacity;
+        }
+    }
+}
