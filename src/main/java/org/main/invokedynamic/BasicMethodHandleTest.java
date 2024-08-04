ed4f967d8544e9e1981a56e75e04adb2215c4490
남의영 <ny2485@naver.com>
2024-08-04 18:30:04 +0900

    invokedynamic test

---
+++ src/main/java/org/main/invokedynamic/BasicMethodHandleTest.java
@@ -0,0 +1,24 @@
+package org.main.invokedynamic;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+
+public class BasicMethodHandleTest {
+    public static void main(String[] args) throws Throwable {
+
+        // public method에 대한 액세스를 제공하는 lookup을 만듦
+        Lookup publicLook = MethodHandles.publicLookup();
+
+        //private, protected method에 대한 액세스를 제공하는 lookup을 만듦
+        Lookup otherLookup = MethodHandles.lookup();
+
+        MethodType methodType = MethodType.methodType(String.class, char.class, char.class);
+        MethodHandle replaceMethodHandle = publicLook.findVirtual(String.class, "replace", methodType);
+
+        String output = (String) replaceMethodHandle.invoke("java", Character.valueOf('o'), 'a');
+//        String output = (String) replaceMethodHandle.invoke("java", Character.valueOf('o'), 'a'); // auth-unboxing을 앞에서 해준다
+        System.out.println("output : "+ output);
+    }
+}
