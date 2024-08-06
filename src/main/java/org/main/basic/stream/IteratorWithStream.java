b3d87b20d8b36c8d3c1be45fad1c8d75c2255f00
남의영 <ny2485@naver.com>
2024-08-07 03:35:12 +0900

    examples using stream to provide objects as lambda params

--- src/main/java/org/main/basic/stream/IteratorWithStream.java
+++ src/main/java/org/main/basic/stream/IteratorWithStream.java
@@ -16,9 +16,29 @@ public class IteratorWithStream {
             System.out.println("next : " + next);
         }
 
+        System.out.println("===========");
+
         /** 후 : stream 사용 */
         List<String> listForStream = Arrays.asList("한식", "중식", "양식");
         Stream<String> stream = listForStream.stream();
         stream.forEach(name -> System.out.println("name : " + name));
+
+        System.out.println("===========");
+
+        /** stream으로 객체를 가져와 람다식의 매개값으로 제공 */
+        List<Kitchen> listForMiddleProcessWithLambda = Arrays.asList(
+                new Kitchen("한식", 100),
+                new Kitchen("중식", 50),
+                new Kitchen("양식", 80)
+                );
+
+        listForMiddleProcessWithLambda.stream()
+                .forEach(kitchen -> {
+                    int capacity = kitchen.getCapacity();
+                    String type = kitchen.getType();
+                    System.out.println("type : " + type);
+                    System.out.println("capacity : " + capacity);
+                }
+                );
     }
 }
